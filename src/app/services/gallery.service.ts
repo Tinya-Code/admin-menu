bef7493b9ec31bb7153962386651c27f7c0fe8da
Dennis Miranda <dennise.miranda.15@gmail.com>
2026-07-11 19:17:58 -0600

    refactor(gallery): unwrap nested data responses
    - Handle nested res.data.data structure in paginated responses
    - Unwrap res.data directly for single item operations

--- src/app/services/gallery.service.ts
+++ src/app/services/gallery.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable, inject } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, map } from 'rxjs';
 import { environment } from '../../environments/environment';
 import { ApiResponse } from '../models/api-response';
 import { Gallery } from '../models/gallery';
@@ -20,19 +20,30 @@ export class GalleryService {
       .set('limit', limit)
       .set('order_by', 'date')
       .set('sortDirection', 'ASC');
-    return this.http.get<ApiResponse<Gallery[]>>(this.apiUrl, { params });
+    return this.http.get<any>(this.apiUrl, { params }).pipe(
+      map((res) => ({
+        ...res,
+        data: res.data?.data ?? res.data ?? [],
+      }))
+    );
   }
 
   getById(id: string): Observable<ApiResponse<Gallery>> {
-    return this.http.get<ApiResponse<Gallery>>(`${this.apiUrl}/${id}`);
+    return this.http.get<any>(`${this.apiUrl}/${id}`).pipe(
+      map((res) => ({ ...res, data: res.data ?? res }))
+    );
   }
 
   create(formData: FormData): Observable<ApiResponse<Gallery>> {
-    return this.http.post<ApiResponse<Gallery>>(this.apiUrl, formData);
+    return this.http.post<any>(this.apiUrl, formData).pipe(
+      map((res) => ({ ...res, data: res.data ?? res }))
+    );
   }
 
   update(id: string, formData: FormData): Observable<ApiResponse<Gallery>> {
-    return this.http.put<ApiResponse<Gallery>>(`${this.apiUrl}/${id}`, formData);
+    return this.http.put<any>(`${this.apiUrl}/${id}`, formData).pipe(
+      map((res) => ({ ...res, data: res.data ?? res }))
+    );
   }
 
   delete(id: string): Observable<MessageResponse> {
